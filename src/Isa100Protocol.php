526ec7bfca9fdaae1f13190677528b213ba5f1ab
erik <erik@erik.xyz>
2026-07-22 01:23:31 +0800

    更新版权

--- src/Isa100Protocol.php
+++ src/Isa100Protocol.php
@@ -1,5 +1,9 @@
 <?php
 
+/*
+ * Copyright (c) 2026 erik <erik@erik.xyz> — https://erik.xyz
+ */
+
 namespace Erikwang2013\IndustrialProtocols\Isa100;
 
 use Erikwang2013\IndustrialProtocols\Bridge\BridgeConnector;
